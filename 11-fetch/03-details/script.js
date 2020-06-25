c76cbc91746a52e5227989cb48ec878398ed28a4
becode <mehdi.semlali.1988@gmail.com>
2020-06-25 13:52:11 +0200

    rien

--- 11-fetch/03-details/script.js
+++ 11-fetch/03-details/script.js
@@ -9,31 +9,27 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 (() => {
-
-    async function charlesxavier() {
-
+    async function charlexavier() {
         let api = await fetch("http://localhost:3000/heroes");
-        charlesxavier = await api.json();
-        var xmenID = document.getElementById("hero-id").value;
-        console.log(xmenID); 
+        let xList = await api.json();
 
+        let userId = document.getElementById("hero-id").value;
+        let xmen = xList[userId - 1];
 
-        // document.getElementById("target").innerHTML = 
-        
-        // `<li class="hero">
-        //     <h4 class="title">
-        //         <strong class="name">${xmen.name}</strong>
-        //         <em class="alter-ego">${xmen.alterEgo}</em>
-        //     </h4>
-        //     <p class="powers">${xmen.abilities}</p>
-        // </li>`
-
-
+        if (userId <= xList.length) {
+            document.getElementById("target").innerHTML =
+                `<li class="hero">
+                    <h4 class="title">
+                        <strong class="name">${xmen.name}</strong>
+                        <em class="alter-ego">${xmen.alterEgo}</em>
+                    </h4>
+                    <p class="powers">${xmen.abilities}</p>
+                </li>`
+        } else {
+            console.error("Cette ID n'existe pas");
+        }
     }
     document.getElementById("run").addEventListener("click", () => {
-
-        charlesxavier()
-
+        charlexavier();
     })
-
 })();
